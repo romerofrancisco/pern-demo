833782c060f4fe5d576a87c720c0daccdd6c5c85
Francisco Romero <fjromerop@gmail.com>
2020-08-06 18:23:51 -0400

    added db routers and middlewares

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const pool = require('./db');
+
+// const sendQuery = async (query_string) => {
+//     pool.query(query_string, (err, res) => {
+//     if (err) throw err;
+//     pool.end();
+//     return res.rows
+//   });
+// }
+
+//middleware
+app.use(cors());
+app.use(express.json()); //this accesss to request.body and returns json data
+
+//routes//
+//------------------------//
+
+//Testing INSERT
+// app.post('/report', async (req, res) => {
+//     try {
+//         const {
+//             department,
+//             division
+//         } = req.body;
+//         const newEmployee = await pool.query(`INSERT INTO departments VALUES('${department}', '${division}') RETURNING *;`);
+//         res.json('record added');
+//     } catch (err) {
+//         console.error(err.message)
+//     }
+// })
+
+//get all employees
+
+app.get('/report', async (req, res) => {
+    try {
+        const employees = await pool.query('SELECT * FROM employees;')
+        res.json(employees.rows)
+    } catch (err) {
+        console.error(err.message)
+    }
+});
+
+
+//get a employee
+
+app.get('/report/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const employee = await pool.query(`SELECT * FROM employees WHERE employee_id = '${id}';`);
+        res.json(employee.rows[0]);
+    } catch (err) {
+        console.error(err.message)
+    }
+});
+
+//update an employee
+//app.put()
+
+//delete an employee
+//app.delete()
+
+
+
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+};
+app.listen(port, () => {
+    console.log(`Server has started on port: ${port}`);
+});
